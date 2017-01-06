c125da5c2af00e7619aa077d88a4f2016bb5dd8c
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-01-06 12:08:30 +0100

    separation methos post et get

--- src/LOUVRE/TicketBundle/Controller/ReservationController.php
+++ src/LOUVRE/TicketBundle/Controller/ReservationController.php
@@ -20,28 +20,29 @@ class ReservationController extends Controller
 {
     public function reservationAction(Request $request)
     {
-    
-        
+
         $billet= new Billet();
         $billet->setDatedevisite(new \Datetime('now', new \DateTimeZone('Europe/Paris')));//prérempli le champs date de visite avec la date d'aujourd'hui
-      
-        $form = $this->get('form.factory')->create(BilletType::class, $billet);
-        
-        
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) { 
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($billet);
-            $em->flush();
-              
-            return $this->redirectToRoute('louvre_ticket_commandepage', [
+        $form = $this->get('form.factory')->create(BilletType::class, $billet);
+
+        if ($request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+
+            if($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($billet);
+                $em->flush();
+
+                return $this->redirectToRoute('louvre_ticket_commandepage', [
                 'id'=>$billet->getId(),
-            ]);
-                        
+                ]);
+            }
         }
-        
+
         return $this->render('LOUVRETicketBundle:Ticket:form.html.twig', [
-            'form' => $form->createView(),
+        'form' => $form->createView(),
         ]);
     }
 
@@ -52,14 +53,12 @@ class ReservationController extends Controller
         $billet=  $em->getRepository('LOUVRETicketBundle:Billet')->find($id);
 
         if (is_null($billet)) {
-             throw new NotFoundHttpException("La réservation d'id {$id} n'existe pas.");
+            throw new NotFoundHttpException("La réservation d'id {$id} n'existe pas.");
         }
 
         $em->remove($billet);
         $em->flush();
 
         return $this->redirectToRoute('louvre_ticket_reservationpage');
-
     }
-  
 }
